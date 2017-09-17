b5865daab721d09b959a4351e99d168875375278
Aras <arkadiuszchr@gmail.com>
2017-09-17 23:09:42 +0200

    ViewModel, Kategorie, kursy, bestsellery

--- KursMVC/Controllers/HomeController.cs
+++ KursMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KursMVC.ViewModels;
 
 namespace KursMVC.Controllers
 {
@@ -14,9 +15,20 @@ namespace KursMVC.Controllers
 
         public ActionResult Index()
         {
-            var listaKategorii = db.Kategorie.ToList();
+            var kategorie = db.Kategorie.ToList();
 
-            return View();
+            var nowosci = db.Kursy.Where(a => !a.Ukryty).OrderByDescending(a => a.DataDodania).Take(3).ToList();
+
+            var bestsellery = db.Kursy.Where(a => !a.Ukryty && a.Bestseller).OrderBy(a => Guid.NewGuid()).Take(3).ToList();
+
+            var vm = new HomeViewModel()
+            {
+                Kategorie = kategorie,
+                Nowosci = nowosci,
+                Bestsellery = bestsellery
+            };
+
+            return View(vm);
         }
 
         public ActionResult StronyStatyczne(string nazwa)
---
+++ KursMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KursMVC.Models;
+
+namespace KursMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Kategoria> Kategorie { get; set; }
+        public IEnumerable<Kurs> Nowosci { get; set; }
+        public IEnumerable<Kurs> Bestsellery { get; set; }
+
+
+    }
+}
