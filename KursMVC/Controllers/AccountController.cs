77292db38c9efeca91ebf6f1d058cc5e324ad6ca
Aras <arkadiuszchr@gmail.com>
2017-10-02 22:44:15 +0200

    Login mvc

---
+++ KursMVC/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using KursMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KursMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index","Home");
+            }
+            
+        }
+    }
+}
---
+++ KursMVC/ViewModels/AccountViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KursMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzic e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamietaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} musi mieć conajmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+
+        [Compare("Password", ErrorMessage = "Hasło nie jest takie same.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź Hasło")]
+        public string ConfirmPassword { get; set; }
+    }
+}
